ad6611af9062897b5bf9dc4bd1b707dd8199f9b9
Jacksen Nillson <nillsonjacksen@gmail.com>
2023-11-10 11:16:27 +0800

    add code comments to my script.js for improved readability

--- js/script.js
+++ js/script.js
@@ -1,23 +1,36 @@
-document.querySelectorAll('details').forEach(details => {
-    const summary = details.querySelector('summary');
-    const icon = summary.querySelector('.custom-icon');
+// Function that controls the opening and closing of an accordion section
+function toggleAccordion(details) {
+  // Check if the details element is currently closed
+  const isOpening = !details.hasAttribute('open');
+  // Find the icon element within the details element
+  const icon = details.querySelector('.custom-icon');
 
-    summary.addEventListener('click', event => {
-      // Close all other details elements.
-      document.querySelectorAll('details').forEach(otherDetails => {
-        if (otherDetails !== details) {
-          otherDetails.removeAttribute('open');
-          otherDetails.querySelector('.custom-icon').style.transform = 'rotate(0deg)';
-        }
-      });
-
-      // Delay to let the 'open' attribute get toggled before checking the state.
-      window.setTimeout(() => {
-        if (details.hasAttribute('open')) {
-          icon.style.transform = 'rotate(180deg)';
-        } else {
-          icon.style.transform = 'rotate(0deg)';
-        }
-      }, 0);
-    });
+  // Iterate over all details elements to close any that are open
+  document.querySelectorAll('details').forEach(otherDetails => {
+    if (otherDetails !== details) {
+      otherDetails.removeAttribute('open');
+      otherDetails.querySelector('.custom-icon').style.transform = 'rotate(0deg)';
+    }
   });
+
+  // Determine if the current details element should open or close
+  if (isOpening) {
+    details.setAttribute('open', '');
+    icon.style.transform = 'rotate(180deg)';
+  } else {
+    details.removeAttribute('open');
+    icon.style.transform = 'rotate(0deg)';
+  }
+}
+
+// Attach a click event listener to the accordion wrapper for event delegation
+document.querySelector('.accordion-wrapper').addEventListener('click', event => {
+  // Find the nearest summary element from the clicked target
+  const summary = event.target.closest('summary');
+  if (summary) {
+    // Prevent default to stop the browser from toggling 'open' automatically
+    event.preventDefault(); // Prevent the default summary click action
+    const details = summary.parentNode; // Reference the parent details of the clicked summary
+    toggleAccordion(details); // Call the toggle function to open or close the accordion
+  }
+});
